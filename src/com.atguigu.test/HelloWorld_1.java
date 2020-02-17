494fa0a4a8753f9e344202db70a714cde6931d06
CSer2294365315 <2294365315@qq.com>
2020-02-17 20:01:46 +0800

    Cser229本人 20：00

--- src/com.atguigu.test/HelloWorld_1.java
+++ src/com.atguigu.test/HelloWorld_1.java
@@ -1,7 +1,12 @@
 package com.atguigu.test;
 
 public class HelloWorld_1 {
+    /**
+     * Cser本人 code 20：00
+     * @param args
+     */
     public static void main(String[] args) {
         System.out.println("hello,world-1");
+        System.out.println("this is 20:00 of Cser本人");
     }
 }
